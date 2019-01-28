a431551508ce17ff4d0de552bc8c581e2440917d
Kevin K <mungaikamau7@gmail.com>
2019-01-28 10:32:13 +0300

    few changes

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [com.stuartsierra/component "0.3.2"]
                  [io.pedestal/pedestal.service       "0.5.5"]
-                 ;; [io.pedestal/pedestal.service-tools "0.5.5"] ;; Only needed for ns-watching; WAR tooling
                  [io.pedestal/pedestal.jetty         "0.5.5"]
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.25"]
@@ -15,8 +14,7 @@
                  [io.pedestal/pedestal.interceptor "0.5.5"]
                  [io.pedestal/pedestal.log "0.5.5"]
                  [io.pedestal/pedestal.route "0.5.5"]
-                 
-                 [com.grzm/component.pedestal "0.1.7"];; pedestal component wrapper
+                 [com.grzm/component.pedestal "0.1.7"]
                  ]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
                                   [com.stuartsierra/component.repl "0.2.0"]
